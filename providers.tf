ad4d88e83205b27b5a7a7d89bb9a7888bc3d06be
ayanny <amir_yanni@yahoo.com>
2023-03-20 23:44:22 -0600

    Apply initial configuration

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 4.37.0"
+    }
+    tls = {
+        source = "hashicorp/tls"
+        version = ">= 4.0.3"
+    }
+    cloudinit = {
+        source = "hashicorp/cloudinit"
+        version = ">=2.2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+        Environment = var.environment
+    }
+  }
+}
