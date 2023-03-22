070f159f5032ec704be2d240fb1b129878035dc6
ayanny <amir_yanni@yahoo.com>
2023-03-21 19:42:58 -0600

    Complete networking configurations.

---
+++ instance_loadbalancing.tf
@@ -0,0 +1,24 @@
+# Loadbalancing is an important requirement for any Biz. H/A is a requirement for Rainpole Inc.
+# Rainpole will use Multiple Load Balancers to access the different applications configured.
+# There will be two types of load balancers, public facing and internal load-balancers.
+# Web Server will use a public load-balancer to receive traffic from internet and send it to multiple instances of Web Servers
+# All other application servers (Application, Cache and Data) will use internal load-balancers.
+
+# Load-Balancers will require a set of resources, this include, Internet GW for internet traffic ingress
+# a NAT GW to allow communication between all internal servers
+# Elastic IPs for external load balancer and NAT GW
+# Load Balancers Target groups and listeners
+# Route Tables for routing of traffic and associatation of instances to target groups.
+
+
+# Let's first create External LB with public IP@
+resource "aws_lb" "ext_lb" {
+  name = "External Load Balancer"
+  load_balancer_type = "network"
+  enable_cross_zone_load_balancing = true
+
+  subnet_mapping {
+    subnet_id = aws_subnet.external_Access.id
+    allocation_id = 
+  }
+}
--- instance_networking.tf
+++ instance_networking.tf
@@ -13,7 +13,6 @@ resource "aws_vpc" "rainpole_vpc" {
   }
 }
 
-
 # We will need to create and define Networking subnets, subnets are required for inter-vpc communication
 
 resource "aws_subnet" "external_Access" {
@@ -44,4 +43,92 @@ resource "aws_subnet" "data_subnet" {
   vpc_id            = aws_vpc.rainpole_vpc.id
   cidr_block        = "10.0.4.0/24"
   availability_zone = data.aws_availability_zone.available.name[0]
+}
+
+# We will need to Create the following resources for Network communications
+# Internet GW for internet access
+# NAT GW for internal instance communication 
+# EIP for NAT GW and Public EIP for Public-Facing LB
+
+# First Create External Elastic IP
+resource "aws_eip" "external_eip" {
+  vpc = true
+}
+
+# Second, create EIP for NAT GW
+resource "aws_eip" "nat_gw_eip" {
+  vpc = true
+}
+
+# Create Internet GW
+resource "aws_internet_gateway" "internet_gw" {
+  vpc_id = aws_vpc.rainpole_vpc.id
+
+  tags = {
+    "Name" = "Internet_GW"
+  }
+}
+
+# Create NAT GW
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_gw_eip.id
+  subnet_id = aws_subnet.external_Access.id
+  depends_on = [
+    aws_internet_gateway.internet_gw
+  ]
+
+  tags = {
+    "Name" = "NAT_GW"
+  }
+}
+
+# Now we need to configure routing, routing requires route tables.
+# Create RT for External traffic
+
+resource "aws_route_table" "external_subnets_rt" {
+  vpc_id = aws_vpc.rainpole_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gw.id
+  }
+}
+
+# Create RT for NAT GW, NAT GW will receive a quad-zero as a default route
+resource "aws_route_table" "nat_gw_subnets_rt" {
+  vpc_id = aws_vpc.rainpole_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+}
+
+# After Creating RTs, we need to associate RTs with Servers and Resources
+
+resource "aws_route_table_association" "nat_gw__RTassociation__web_server" {
+  subnet_id = aws_subnet.web_subnet.id
+  route_table_id = aws_route_table.nat_gw_subnets_rt.id
+}
+
+resource "aws_route_table_association" "nat_gw__RTasspocation_app_server" {
+  subnet_id = aws_subnet.app_subnet.id
+  route_table_id = aws_route_table.nat_gw_subnets_rt.id
+}
+
+resource "aws_route_table_association" "nat_gw__RTasspocation_cache_server" {
+  subnet_id = aws_subnet.cache_subnet.id
+  route_table_id = aws_route_table.nat_gw_subnets_rt.id
+}
+
+resource "aws_route_table_association" "nat_gw__RTasspocation_data_server" {
+  subnet_id = aws_subnet.data_subnet.id
+  route_table_id = aws_route_table.nat_gw_subnets_rt.id
+}
+
+# Finally create RT associations for Extenral subnets
+
+resource "aws_route_table_association" "external_traffic_route_association" {
+  subnet_id = aws_subnet.external_Access.id
+  route_table_id = aws_route_table.external_subnets_rt.id
 }
