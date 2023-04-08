785f293cdba64e09ebc73e860554ba47f5b5ce5f
ayanny <amir_yanni@yahoo.com>
2023-04-08 20:28:44 +0100

    fix errors

--- providers.tf
+++ providers.tf
@@ -16,8 +16,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = data.vault_aws_access_credentials.aws.access_key
-  secret_key = data.vault_aws_access_credentials.aws.secret_key
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
   region = var.aws_region
   default_tags {
     tags = {
@@ -34,7 +34,38 @@ provider "vault" {
   token   = var.vault_token
 }
 
-data "vault_aws_access_credentials" "aws" {
-  path = "aws/creds/my-role"
+# data "vault_aws_access_credentials" "aws" {
+#   path = "aws/creds/my-role"
+# }
+
+
+
+
+resource "vault_aws_secret_backend" "aws" {
+  access_key = var.vault_access_key
+  secret_key = var.vault_secret_key
 }
 
+resource "vault_aws_secret_backend_role" "role" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "my-role2"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+# generally, these blocks would be in a different module
+data "vault_aws_access_credentials" "creds" {
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.role.name
+}
