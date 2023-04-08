e92f626284845f727c08952a3b8be183e7e79799
ayanny <amir_yanni@yahoo.com>
2023-04-08 17:43:23 +0100

    fix errors

--- providers.tf
+++ providers.tf
@@ -16,8 +16,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = data.vault_generic_secret.vault.access_key
-  secret_key = data.vault_generic_secret.vault.secret_key
+  access_key = data.vault_generic_secret.access_key
+  secret_key = data.vault_generic_secret.secret_key
   region = var.aws_region
   default_tags {
     tags = {
