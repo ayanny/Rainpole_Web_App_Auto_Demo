1c2a0b588cf728f48369e4658f319458453713fc
ayanny <amir_yanni@yahoo.com>
2023-04-08 19:47:50 +0100

    fix error

--- providers.tf
+++ providers.tf
@@ -29,13 +29,12 @@ provider "aws" {
 
 provider "vault" {
 #  address   = local.hcp_vault_public_endpoint
-  address = "https://rainpole-vault-cluster-public-vault-189353e2.b5e88078.z1.hashicorp.cloud:8200/ui/vault/secrets?namespace=admin"
+  address = "https://rainpole-vault-cluster-public-vault-189353e2.b5e88078.z1.hashicorp.cloud"
   token   = var.vault_token
 #  namespace = local.hcp_vault_namespace
 
 }
 
 data "vault_generic_secret" "aws" {
-  backend = "aws"
   path = "aws/creds/my-role"
 }
